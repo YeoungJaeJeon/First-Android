7276cfbc31cad8c4fbb5d321f5a7205182dcafa8
YeoungJaeJeon <won01414@gmail.com>
2019-07-24 21:06:52 +0900

    Kakao copy

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,23 +11,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        Button loginButton = findViewById(R.id.loginButton);
-        Log.d("영재",""+loginButton.getId());
-        Log.e("영재",""+loginButton.getId());
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "로그인버튼을 눌렀습니다", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        Button facebookButton = findViewById(R.id.facebookButton);
-        facebookButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "페이스북버튼을 눌렀습니다", Toast.LENGTH_SHORT).show();
-            }
-        });
+        setContentView(R.layout.activity_kakao);
     }
 }
